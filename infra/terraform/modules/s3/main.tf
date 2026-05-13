c813038da60b168af11635d5e0a17be0fed74491
ifeoluwashola <dloveade@gmail.com>
2026-05-13 15:50:14 +0100

    fix: correct variable name references in S3 bucket configuration

--- infra/terraform/modules/s3/main.tf
+++ infra/terraform/modules/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "academy_uploads" {
-  bucket = "${va.project_name}-academy-uploads-${va.environment}"
+  bucket = "${var.project_name}-academy-uploads-${var.environment}"
 }
 
 resource "aws_s3_bucket_ownership_controls" "academy_uploads_ownership" {
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_cors_configuration" "academy_uploads_cors" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["PUT", "POST", "GET"]
-    allowed_origins = va.allowed_origins
+    allowed_origins = var.allowed_origins
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
